a16738b9c6df284d6fbd77699980d8999fd1feda
f <fan.ke@okg.com>
2023-04-21 16:23:01 +0800

    supoort gasfee when run task

--- src/lib/ExchainClientExtra.ts
+++ src/lib/ExchainClientExtra.ts
@@ -4,6 +4,7 @@ import {
 import { OfflineAminoSigner } from '@cosmjs/amino/build/signer';
 import { ContractRef } from '../config';
 import { DefaulrGasPrice } from './key';
+import { StdFee } from '@cosmjs/stargate';
 
 export type ContractRefs = { [contractName: string]: ContractRef };
 
@@ -27,22 +28,22 @@ export class ExchainClientExtra {
     contract: string,
     wallet: OfflineAminoSigner,
     msg: Record<string, any>,
+    gasfee: StdFee | 'auto' | number,
     funds?: Coin[],
     instanceId = 'default',
   ): Promise<ExecuteResult> {
-
     const cosmwasmClient = await SigningCosmWasmClient.connectWithSigner(
       this.httpEndpoint,
       wallet,
       {
         gasPrice: DefaulrGasPrice,
         broadcastTimeoutMs: 60_000,
-        broadcastPollIntervalMs: 60_000,
+        broadcastPollIntervalMs: 10_000,
       },
     );
     const account = await wallet.getAccounts();
     const contractAddress = contract.startsWith('ex') || contract.startsWith('0x') ? contract : this.refs[contract].contractAddresses[instanceId];
-    const res = await cosmwasmClient.execute(account[0].address, contractAddress, msg, 'auto', 'execute', funds);
+    const res = await cosmwasmClient.execute(account[0].address, contractAddress, msg, gasfee, 'execute', funds);
     return res;
   }
 }
